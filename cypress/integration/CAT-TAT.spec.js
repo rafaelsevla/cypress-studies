ec47b7803d18b7bb11f00ceca015785024977f34
Rafael Costa <rafaelsevla@gmail.com>
2024-02-21 21:45:31 -0300

    Add test to fill form

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -1,9 +1,23 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-  it('verifica o título da aplicação', function() {
+  beforeEach(() => {
     cy.visit('./src/index.html')
+  })
 
+  it('verifica o título da aplicação', () => {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it('preenche os campos obrigatórios e envia o formulário', () => {
+    const longText = 'Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste!'
+
+    cy.get('#firstName').type('Peter')
+    cy.get('#lastName').type('Parker')
+    cy.get('#email').type('peter.parker@gmail.com')
+    cy.get('#open-text-area').type('Vai teia')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
   })
 })
