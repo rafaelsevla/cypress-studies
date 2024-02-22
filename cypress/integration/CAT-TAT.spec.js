23e27047b6582e79ea6bc965e7237f3350f7af78
Rafael Costa <rafaelsevla@gmail.com>
2024-02-21 22:07:23 -0300

    Add all first tests and create customized command

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -30,4 +30,57 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     cy.get('.error').should('be.visible')
   })
+
+  it('digitar um texto no campo de telefone e continuar vazio', () => {
+    cy.get('#phone').type('Estou digitando um texto aqui e vejo que nao altera pq sera deve ser pq é um telefone nao é mesmo hahahaha eu me divirto com esse diario', { delay: 0 }).should('have.value', '')
+  })
+
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').type('Peter')
+    cy.get('#lastName').type('Parker')
+    cy.get('#email').type('peter.parker.gmail.com')
+    cy.get('#phone-checkbox').click()
+    cy.get('#open-text-area').type('Vai teia!')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+  })
+
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy.get('#firstName')
+      .type('Peter')
+      .should('have.value', 'Peter')
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#lastName')
+      .type('Parker')
+      .should('have.value', 'Parker')
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#email')
+      .type('peter.parker.gmail.com')
+      .should('have.value', 'peter.parker.gmail.com')
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#phone')
+      .type('123456789')
+      .should('have.value', '123456789')
+      .clear()
+      .should('have.value', '')
+  })
+
+  it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+  })
+
+  it('envia o formuário com sucesso usando um comando customizado', () => {
+    cy.fillMandatoryFieldsAndSubmit()
+
+    cy.get('.success').should('be.visible')
+  })
 })
