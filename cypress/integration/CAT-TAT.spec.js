2b4a9f23810a0cc9a27a9d499e846c0aeac6e479
Rafael Costa <rafaelsevla@gmail.com>
2024-02-26 21:23:06 -0300

    Add test for checkbox and radio

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -35,11 +35,11 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     cy.get('#phone').type('Estou digitando um texto aqui e vejo que nao altera pq sera deve ser pq é um telefone nao é mesmo hahahaha eu me divirto com esse diario', { delay: 0 }).should('have.value', '')
   })
 
-  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+  it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
     cy.get('#firstName').type('Peter')
     cy.get('#lastName').type('Parker')
     cy.get('#email').type('peter.parker.gmail.com')
-    cy.get('#phone-checkbox').click()
+    cy.get('#phone-checkbox').check()
     cy.get('#open-text-area').type('Vai teia!')
     cy.contains('button', 'Enviar').click()
 
@@ -82,5 +82,39 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     cy.fillMandatoryFieldsAndSubmit()
 
     cy.get('.success').should('be.visible')
+  })
+
+  it('seleciona um produto (YouTube) por seu texto', () => {
+    cy.get('#product').select('YouTube').should('have.value', 'youtube')
+  })
+
+  it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+    cy.get('#product').select('mentoria').should('have.value', 'mentoria')
+  })
+
+  it('seleciona um produto (Blog) por seu índice', () => {
+    cy.get('#product').select(1).should('have.value', 'blog')
+  })
+
+  it('marca o tipo de atendimento "Feedback"', () => {
+    cy.get('input[type="radio"][value="feedback"]').check().should('have.value', 'feedback')
+  })
+
+  it('marca cada tipo de atendimento', () => {
+    cy.get('input[type="radio"]')
+      .should('have.length', 3)
+      .each(($radio,) => {
+        cy.wrap($radio).check()
+        cy.wrap($radio).should('be.checked')
+      })
+  })
+
+  it('marca ambos checkboxes, depois desmarca o último', () => {
+    cy.get('input[type="checkbox"]')
+      .check()
+      .should('be.checked')
+      .last()
+      .uncheck()
+      .should('be.not.checked')
   })
 })
